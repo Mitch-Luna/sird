1f553bd0e3ba8e17bbf32a6c7d7f66ce8d17c2fa
Mitch-Luna <lunitamitch02@gmail.com>
2024-07-10 15:42:59 -0600

    modulo de sexo y nivel academico terminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
 import { ProfessionsModule } from './modules/professions/profession.module';
 import { GradesModule } from './modules/grades/grades.module';
+import { GenderModule } from './modules/gender/gender.module';
+import { AcademicLevelModule } from './modules/academiclevel/academiclevel.module';
 
 @Module({
   imports: [
@@ -14,7 +16,10 @@ import { GradesModule } from './modules/grades/grades.module';
     }),
     DatabaseModule,
     ProfessionsModule,
-    GradesModule
+    GradesModule,
+    GenderModule,
+    AcademicLevelModule
+
   ],
   controllers: [AppController],
   providers: [AppService],
